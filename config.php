5d800579ebce1664e9d09b2abf4f348ede208b5c
PriyamAgrawal <priyamagrawal2208@gmail.com>
2018-09-07 01:08:45 +0530

    database setup

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
-// $dbhost="103.50.162.66";
-// $dbuser="kodamr13_ieee";
-// $dbpass="hatemane@1";
-// $dbname="kodamr13_ieeemanipal";
-// $connection=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-// if(mysqli_connect_errno()){
-// 	die("Database cant load"); 
-// }
-$dbhost="localhost";
-$dbuser="root";
-$dbpass="qwerty123456";
-$dbname="decrypt";
+$dbhost="103.50.162.66";
+$dbuser="kodamr13_ieee";
+$dbpass="hatemane@1";
+$dbname="kodamr13_ieeemanipal";
 $connection=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 if(mysqli_connect_errno()){
 	die("Database cant load"); 
 }
+// $dbhost="localhost";
+// $dbuser="root";
+// $dbpass="qwerty123456";
+// $dbname="decrypt";
+// $connection=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+// if(mysqli_connect_errno()){
+// 	die("Database cant load"); 
+// }
 ?>
